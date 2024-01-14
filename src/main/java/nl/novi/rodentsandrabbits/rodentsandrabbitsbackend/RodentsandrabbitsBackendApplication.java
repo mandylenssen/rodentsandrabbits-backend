b85c73d51665dc57243169595c5afcbd07c4bf0a
mandylenssen <mandylenssen@gmail.com>
2024-01-14 13:13:24 +0100

    intial setup

---
+++ src/main/java/nl/novi/rodentsandrabbits/rodentsandrabbitsbackend/RodentsandrabbitsBackendApplication.java
@@ -0,0 +1,13 @@
+package nl.novi.rodentsandrabbits.rodentsandrabbitsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RodentsandrabbitsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RodentsandrabbitsBackendApplication.class, args);
+	}
+
+}
