d8feadd3a5320081a8bbae190870b706f5ce4270
mandylenssen <mandylenssen@gmail.com>
2024-02-09 11:31:53 +0100

    data.sql wachtwoord aangepast

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO users (username, password, first_name, last_name, phone_number, enabled, email) VALUES ('mandy@gmail.com','$2a$10$5aSpwjpUHv5bvw84hUyOc.KSNrMhnbKyL2q6fWGF8f6HVDAmQY8QC', 'Mandy', 'Lenssen', '0612229522', TRUE, 'mandy@gmail.com');
+INSERT INTO users (username, password, first_name, last_name, phone_number, enabled, email) VALUES ('mandy@gmail.com','$2a$10$7Q1mCx4OYCDlTRkTQyJyBeHLtB7TMrQK3DtxEfoUWfh8ep8OdFNde', 'Mandy', 'Lenssen', '0612229522', TRUE, 'mandy@gmail.com');
 
 INSERT INTO pets (name, birthday, species, gender, details, diet, owner) VALUES ('Squeek', '2019-01-01', 'rat', 'male', 'cute rat', 'pellets', 'mandy@gmail.com'),
                                                                                     ('Whiskers', '2019-03-21', 'rat', 'male', 'silly rat', 'pellets', 'mandy@gmail.com'),
